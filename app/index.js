f1973ef9c2b6bf20e75dee95c644e01e05630f81
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-10-16 22:54:23 -0500

    patch react tap events

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 import AppProvider from './router';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import './index.scss';
+
+injectTapEventPlugin();
 
 ReactDOM.render(<AppProvider/>, document.getElementById('content'));

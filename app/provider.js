bda75f63d4922393bcc9d7e6a6530e0a21c4e8b2
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-10-16 22:54:01 -0500

    basic styles

--- app/provider.js
+++ app/provider.js
@@ -1,10 +1,28 @@
 import React, {PropTypes} from 'react';
+import AppBar from 'material-ui/AppBar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {Provider} from 'react-redux';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import {getStore} from './store';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+
+const Title = () =>
+  <span>
+    Node
+    <b>Git</b>
+  </span>;
 
 const AppProvider = props =>
   <Provider store={getStore()}>
-    {props.children}
+    <MuiThemeProvider
+      muiTheme={getMuiTheme(lightBaseTheme)}
+      showMenuIconButton={false}
+    >
+      <div>
+        <AppBar title={<Title/>}/>
+        {props.children}
+      </div>
+    </MuiThemeProvider>
   </Provider>;
 
 AppProvider.displayName = 'AppProvider';

f01bc7e9bfdecfe269cdc376f680851ef56549d0
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-10-26 19:47:45 -0500

    get list of repositories

--- api/repository/index.js
+++ api/repository/index.js
@@ -1,28 +1,44 @@
 import {exec} from 'child_process';
 
-export default {
-  POST: (request, reply) => {
-    if (!request.payload || !request.payload.name) {
-      const response = reply();
+function getDefaultPayload(request) {
+  return Object.assign({}, request.payload, {
+    name: 'repo'
+  });
+}
+
+const GET = (request, reply) => {
+  const payload = getDefaultPayload(request);
+
+  exec(`node-git list --name ${payload.name} --json`, (err, stdout) => {
+    if (err) {
+      const response = reply(err.toString());
 
       response.statusCode = 500;
-
-      return;
+      response.type('text/plain');
+    } else {
+      reply(stdout);
     }
+  });
+};
 
-    const repo = request.payload.name;
+const POST = (request, reply) => {
+  const payload = getDefaultPayload(request);
 
-    exec(`node-git create --name ${repo}`, err => {
-      if (err) {
-        const response = reply(err.toString());
+  exec(`node-git create --name ${payload.name}`, err => {
+    if (err) {
+      const response = reply(err.toString());
 
-        response.statusCode = 500;
-        response.type('text/plain');
-      } else {
-        reply(JSON.stringify({
-          message: `Successfully created ${repo} repository`
-        }));
-      }
-    });
-  }
+      response.statusCode = 500;
+      response.type('text/plain');
+    } else {
+      reply(JSON.stringify({
+        message: `Successfully created ${repo} repository`
+      }));
+    }
+  });
+};
+
+export default {
+  GET,
+  POST
 };
